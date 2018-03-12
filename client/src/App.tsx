1e843d678c7449b25a8b8751ac30f6e7ed654350
hanselrd <hansel809@hotmail.com>
2018-03-12 06:16:00 +0000

    Added three.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import './App.css';
-import Chat from './Chat';
+// import Chat from './Chat';
+import Game from './Game';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <p>Darkve</p>
-        <Chat />
+        {/* <p>Darkve</p> */}
+        {/* <Chat /> */}
+        <Game />
       </div>
     );
   }
